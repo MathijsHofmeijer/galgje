df84fcf902c64b86d162db52c7777b1973206064
MathijsHofmeijer <mathijshofmeijer@gmail.com>
2021-01-14 14:11:55 +0100

    galgje klaar

--- javascript/js.js
+++ javascript/js.js
@@ -7,12 +7,12 @@ var totalRightLetters = 0;
 var chosenLetters;
 var guessedLetters = [];
 
-/*staten = ['Alabama','Alaska','Arizona','Arkansas','Californië' ,'Colorado','Connecticut','Delaware','Florida','Georgia','HawaiiIdaho','Illinois',
+staten = ['Alabama','Alaska','Arizona','Arkansas','Californië' ,'Colorado','Connecticut','Delaware','Florida','Georgia','HawaiiIdaho','Illinois',
      'Indiana','Iowa','Kansas','Kentucky', 'Louisiana','Maine','Maryland','Massachusetts', 'Michigan','Minnesota','Mississippi','Missouri','Montana',
      'Nebraska','Nevada' ,'New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania',
      'Rhode Island','South Carolina','South Dakota','Tennessee','texas','Utah','Vermont','Virginia','Washington','Washington DC','West Virginia','Wisconsin','Wyoming']
-*/
-staten = ['utah'];
+
+
 alfabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var randomWordPicker = Math.floor(Math.random() * staten.length);
@@ -32,55 +32,42 @@ function startgame(){
 }
 startgame();
 
-var Buttonss = document.getElementById("alphabet");
+var alphaButton = document.getElementById("alphabet");
 
-Buttonss.addEventListener("click", returnChosenLetter);
+alphaButton.addEventListener("click", returnChosenLetter);
 
 function returnChosenLetter(letter){
     if (letter.target !== letter.currentTarget) {
-        checkLetter(letter.target.id);
+        checkchosenletter(letter.target.id);
     }
 }
-
-function checkLetter(chosenLetters) {
-    var chosenbutton = document.getElementById(chosenLetters);
-
-    if (chosenbutton.classList.contains("active") === false){
-        chosenbutton.classList.add("active");
-        totalguesses++;
-        document.getElementById("totalguesses").innerHTML = totalguesses;
-
-        for (var i = 0; i < randomWord.length; i++) {
-            if (randomWord[i].toLowerCase() === chosenLetters){
-                guessedLetters[i].innerHTML = randomWord[i];
-                totalRightLetters += 1;
-            }
-        }
-        checkchosenletter(guessedLetters);
-    }
-    else{
-        alert ("kies een andere letter aub");
-        return false;
-    }
-}
-
 function checkchosenletter(letter){
     var j = (randomWord.toLowerCase().indexOf(letter));
+    console.log(letter);
     if (j === -1){
-        maxlifes --;
+        
+        maxlifes--;
         document.getElementById("maxlifes").innerHTML = maxlifes;
         checkVerlies(maxlifes);
     }
     else{
-        rightguesses++;
-        document.getElementById("rightguesses").innerHTML = rightguesses;
-
-        checkwinst(totalRightLetters);
+        for (var i = 0; i < randomWord.length; i++) {
+            if (randomWord[i].toLowerCase() === letter){
+                guessedLetters[i].innerHTML = randomWord[i];
+                rightguesses += 1;
+                document.getElementById("rightguesses").innerHTML = rightguesses;
+            }
+        }
     }
+    totalguesses++;
+    document.getElementById("totalguesses").innerHTML = totalguesses;
+    checkwinst(rightguesses);
+
 }
 
-function checkwinst(lettersgoed){
-    if(randomWord.length === lettersgoed){
+
+function checkwinst(right){
+    if(randomWord.length === right){
         if( alert('YOU WON')){
         }
         else{
